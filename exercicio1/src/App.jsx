0708fee5963fd5ac87e6834fd6d8c26d8d7328a3
Carlos Cayke B Nakazone <103291511+caykenakazone@users.noreply.github.com>
2023-05-02 18:15:00 -0300

    Add files via upload

---
+++ exercicio1/src/App.jsx
@@ -0,0 +1,12 @@
+import Contador from "./components/Contador";
+
+export default function App() {
+  return (
+    <div>
+      <Contador v1={3} v2={7} />
+      <Contador v1={10} v2={17} />
+    
+    </div>
+  )
+}
+
